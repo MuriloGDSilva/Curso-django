defec8c49448ffff66b62b68d640a23642d776c8
Murilo Gomes Silva <murilo.silva12339@gmail.com>
2023-03-25 02:21:22 -0300

    error adjustment

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -11,15 +11,22 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('id'))
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}-Category ',
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })

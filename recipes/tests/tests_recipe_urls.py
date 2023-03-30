23122a98a6f9feca32e49985195307dbffce6cd0
Murilo Gomes Silva <murilo.silva12339@gmail.com>
2023-03-30 02:55:36 -0300

    testing the application

---
+++ recipes/tests/tests_recipe_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url_home = reverse('recipes:home')
+        self.assertEqual(url_home, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        category_url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        recipe_detail_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(recipe_detail_url, '/recipes/1/')

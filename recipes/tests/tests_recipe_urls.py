b25bdf91e01b2627454e3d00841fa912d533fe76
Murilo Gomes Silva <murilo.silva12339@gmail.com>
2023-04-14 23:55:54 -0300

    Initiate TDD search

--- recipes/tests/tests_recipe_urls.py
+++ recipes/tests/tests_recipe_urls.py
@@ -14,3 +14,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         recipe_detail_url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(recipe_detail_url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        url_search = reverse('recipes:search')
+        self.assertEqual(url_search, '/recipes/search/')

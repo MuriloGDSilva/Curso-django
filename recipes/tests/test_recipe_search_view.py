4a0c123a29996ae1a52ad28d16425edcb1bad5c8
Murilo Gomes Silva <murilo.silva12339@gmail.com>
2023-04-19 21:26:32 -0300

    test_recipe_search_can_find_recipe_by_title

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -23,3 +23,26 @@ class RecipeSearchViewTest(RecipeTestBase):
             reverse('recipes:search') + '?q=TestSearchTerm')
         self.assertIn('Search for &quot;TestSearchTerm&quot;',
                       response.content.decode('utf-8'))
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(slug='one', author_data={
+                                   'username': 'one'}, title=title1)
+        recipe2 = self.make_recipe(slug='two', author_data={
+                                   'username': 'two'}, title=title2)
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        response_both = self.client.get(f'{search_url}?q=This')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertIn(recipe2, response2.context['recipes'])
+
+        self.assertNotIn(recipe1, response2.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])

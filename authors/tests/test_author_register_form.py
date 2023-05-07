2c6942a5cf0fe133b6856128ed2851d36dd1ce2c
Murilo Gomes Silva <murilo.silva12339@gmail.com>
2023-05-07 01:35:17 -0300

    file organization and test login user

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -125,3 +125,19 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         url = reverse('authors:create')
         response = self.client.get(url, self.form_data)
         self.assertEqual(response.status_code, 404)
+
+    def test_author_created_can_login(self):
+        url = reverse('authors:create')
+        self.form_data.update({
+            'username': 'testuser',
+            'password': '@Bc123456',
+            'password2': '@Bc123456',
+        })
+        self.client.post(url, self.form_data, follow=True)
+
+        checking_login = self.client.login(
+            username='testuser',
+            password='@Bc123456'
+        )
+
+        self.assertTrue(checking_login)
